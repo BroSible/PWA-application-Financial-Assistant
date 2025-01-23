f030a0cbfaa6501ead4c6f8462cff4b18fb1ae49
BroSible <mikhail.petr2005@gmail.com>
2025-01-23 22:04:43 +0300

    init

---
+++ PWA application Financial Assistant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PWA_application_Financial_Assistant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
