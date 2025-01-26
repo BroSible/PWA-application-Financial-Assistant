0f4e57751f6fcf5bf10d9234e726404a7c4441b6
BroSible <mikhail.petr2005@gmail.com>
2025-01-26 17:23:32 +0300

    jwt-token
    added generating jwt-token

--- PWA application Financial Assistant/Program.cs
+++ PWA application Financial Assistant/Program.cs
@@ -1,4 +1,41 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+// условная бд с юзерами
+var people = new List<Person>
+{
+    new Person ("mikhail@gmail.com", "1234"),
+    new Person ("ilya@gmail.com", "5678")
+};
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // указывает, будет ли валидироваться издатель при валидации токена
+            ValidateIssuer = true,
+            // строка, представляющая издателя
+            ValidIssuer = AuthOptions.ISSUER,
+            // будет ли валидироваться потребитель токена
+            ValidateAudience = true,
+            // установка потребителя токена
+            ValidAudience = AuthOptions.AUDIENCE,
+            // будет ли валидироваться время существования
+            ValidateLifetime = true,
+            // установка ключа безопасности
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            // валидация ключа безопасности
+            ValidateIssuerSigningKey = true,
+        };
+    });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -14,14 +51,60 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapPost("/login", (Person loginData) =>
+{
+    // находим пользователя
+    Person? person = people.FirstOrDefault(p => p.Email == loginData.Email && p.Password == loginData.Password);
+
+    // если пользователь не найден, отправляем статусный код 401
+    if (person is null)
+    {
+        return Results.Unauthorized();
+    }
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Email) };
+    // создаём JWT-токен
+    var jwt = new JwtSecurityToken(
+            issuer: AuthOptions.ISSUER,
+            audience: AuthOptions.AUDIENCE,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)), // время действия 2 минуты
+            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    //формируем ответ
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = person.Email,
+    };
+
+    return Results.Json(response);
+});
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.Map("/data", [Authorize] (HttpContext context) => $"Hello World!");
+
 app.Run();
+
+public class AuthOptions
+{
+    public const string ISSUER = "FinancialServer"; // Издатель токена
+    public const string AUDIENCE = "FinancialClient"; // Потребитель токена
+    const string KEY = "mysupersecret_secretsecretsecretkey!123"; //  Ключ для шифрования
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
+
+record class Person(string Email, string Password);
